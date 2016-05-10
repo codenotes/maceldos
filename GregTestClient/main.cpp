4309d4fa264abf557cd4ba0681b98329f4ec9a55
Gregory Brill <gbrill@infusiondev.com>
2016-05-10 09:57:25 -0400

    Initial Commit

---
+++ GregTestClient/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GregTestClient
+//
+//  Created by Gregory Brill on 5/10/16.
+//  Copyright © 2016 Gregory Brill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
